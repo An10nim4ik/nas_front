ede5e8ce5d2c9584c56a0aa9b8f943dd542ac1c4
Anonymous <anon@example.com>
2025-04-15 14:01:56 +0300

    Added hh.ru API, parsing vacancies from that API URL

---
+++ scripts/jobs.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', function(){
+    fetchVacancies();
+});
+
+async function fetchVacancies(){
+    try{
+        const response = await fetch("https://api.hh.ru/vacancies?area=40&currency=KZT")
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let data = await response.json();
+        displayData(data.items);
+    }catch  (error){
+        console.error("Error fetching vacancies:" , error);
+    }
+}   
+
+async function displayData(vacancies){
+    const container = document.getElementById("vacancies");
+    container.innerHTML = '';
+
+    vacancies.forEach(vacancy => {
+        const vacancyDiv = document.createElement('div');
+        vacancyDiv.innerHTML = `<h3>${vacancy.name}</h3>
+            <p>Employer: ${vacancy.employer ? vacancy.employer.name : "N/A"}</p>
+            <a href="${vacancy.alternate_url}" target="_blank">View Vacancy</a>`;
+        container.appendChild(vacancyDiv);
+        
+    });
+}
