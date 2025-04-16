056acca9922fcc62bf050a32248ffb9d3be84c61
Anonymous <anon@example.com>
2025-04-16 16:55:15 +0300

    Added news API, and now it's working very well

--- scripts/news.js
+++ scripts/news.js
@@ -14,3 +14,47 @@ const links = document.querySelectorAll('.section-anchor__links a');
       this.classList.add('active');
     });
   });
+
+
+window.addEventListener("DOMContentLoaded", function(){
+  fetchNews();
+});
+
+
+const apikey = "deda703e77e64b8884a17623e6598ff1";
+
+function buildUrl(){
+  let baseUrl = "https://newsapi.org/v2/everything";
+  let API_KEY = apikey;
+  const query = "job market";
+  const url = `${baseUrl}?q=${query}&apiKey=${API_KEY}`; 
+  return url;
+}
+
+async function fetchNews(){
+  try{
+    const url = buildUrl();
+    const response = await fetch(url);
+    if(!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    let data = await response.json();
+    displayData(data.articles);
+  }catch (error){
+    console.error("Error fetching news org: ", error);
+  }
+}
+
+
+async function displayData(articles){
+  const container = document.getElementById("news");
+  container.innerHTML = '';
+
+  articles.forEach(article => {
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `<h1>${article.title}</h1>
+      <p>${article.description}</p>
+      <a href="${article.url}" target="_blank"><div class=link><p>More information</p></div></a>`;
+    container.appendChild(newsDiv);
+  });
+}
